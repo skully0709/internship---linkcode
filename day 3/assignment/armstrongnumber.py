a940be2e29e8039c7b0f817be0ff9906694e42c2
skully0709 <sarthakpatani07@gmail.com>
2026-06-04 10:12:09 +0530

    changes in day 3 and python assignment

--- day 3/assignment/armstrongnumber.py
+++ day 3/assignment/armstrongnumber.py
@@ -2,9 +2,12 @@ n = int(input("Enter a number: "))
 
 temp = n
 sum = 0
+
+
+
 while temp>0:
     digit = temp%10
-    sum = sum + digit**3
+    sum = sum + digit**len(str(n))
     temp = temp//10
 
 if sum == n:
